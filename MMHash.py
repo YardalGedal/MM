0fc2e30dc56d7cff55fb690c988596c5e1779f0d
Arti Markelov <yardalgedal@gmail.com>
2017-08-27 00:27:21 +0300

    Update MMHash.py

--- MMHash.py
+++ MMHash.py
@@ -1,33 +1,20 @@
-# Script Name	: MMHash.py
-# Author		: Yardal Gedal
-# Created		: 08 October 2016
-# Last Modified	: 18 November 2016
-# Version		: 7.0.0
+# Script Name : MMHash.py
+# Author : Yardal Gedal
+# Created : 08 October 2016
+# Last Modified	: 27 August 2017
+# Version : 8.0.0
 
-# Modifications	: 7.0.0
+# Modifications	: 8.0.0
 
-# Description	: Hashing entered string 
+# Description : Hashing input string
+
+from functools import reduce
 
 class MMHash:
-    def __init__(self, hashingstring, hashsize=256):
-        self.hashsize = hashsize
-        self.x = int(hashsize / 16)
-        self.__hashlen = (self.hashsize / 4) + self.x # Length of hash
-        self.hash = self.hashing(hashingstring)
-        
-    def hashing(self, hashingstring):
-        a = self.__exec_hashing_modify(hashingstring)
-        while len(str(a)) < self.__hashlen + self.x:
-            a = a + self.__exec_hashing_modify(a)
-        a = hex(int(a))[self.x:int(self.__hashlen)]
-        return a
-    
-    def __exec_hashing_modify(self, hashingstring):
-        a = ''
-        for s, x in enumerate(hashingstring):
-            a = a + str(self.__hashing_modify(str(x), int(s), len(hashingstring)))
-        return a
-    
-    def __hashing_modify(self, s, x, len_of_hashingstring):
-        n = ord(s) * (x + 1) * len_of_hashingstring * self.hashsize
-        return n
+    def __init__(self, S):
+        x = 256
+        L = (x // 8) * 2
+        f = lambda ords: str(hex(reduce(lambda x, y: int(x)*int(y),  ords) + int("".join(ords))))
+        while len(S) < L:
+            S = f([str((n + x) * x) + str(ord(i)) for n, i in enumerate(S)])
+        print(S[-L//2:])
