056368cc47ba80cad55f3fd1b8ff56affae6ed45
Max Payne <yardalgedal@gmail.com>
2019-04-05 01:46:25 +0300

    Update MMHash.py

--- MMHash.py
+++ MMHash.py
@@ -1,11 +1,13 @@
 from functools import reduce
 
-def MMHash(s: str, x: int = 256):
-    L = (x // 8) * 2
-    f = lambda ords: str(hex(reduce(lambda x, y: int(x) * int(y),  ords) + int("".join(ords))))
-    s = s + str(x)
-    while len(s) < L:
-        s = f(str((n + x) * x) + str(ord(i)) for n, i in enumerate(s))
+def MMHash(s, x: int = 256) -> str:
+    def f(ords) -> str:
+        return str(hex(reduce(lambda prev, curr: int(prev) * int(curr),  ords) + int("".join(ords))))
     
-    return s[-L//2:]
-      
+    L = (x // 8) * 2
+    
+    mmhash = ''
+    while len(mmhash) < L:
+        mmhash = f([str((n + x) * x) + str(ord(i)) for n, i in enumerate(mmhash or (s + str(x)))])
+    
+    return mmhash[-L // 2:]
