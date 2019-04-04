1c2925568974786eacf5771183ddcea144d1c1d0
Max Payne <yardalgedal@gmail.com>
2019-04-05 01:23:18 +0300

    Update MMCrypt.py

--- MMCrypt.py
+++ MMCrypt.py
@@ -1,23 +1,30 @@
-# Script Name : MMCrypt.py
-# Author : Yardal Gedal
-# Created : 09 October 2017
-# Last Modified	: 09 October 2017
-# Version : 1.0.0
-
-# Modifications	: 1.0.0
-
-# Description : Crypt/encrupy input string
+import string
+import random
 
 from math import e
 
 class MMCrypt:
-    
-    def __init__(self):
-        self.SALT = 1/e
-        self.ACCURACY = 1 # bigger is better, only integer
+    def __init__(self, salt: (int, float) = e, accuracy: int = 1):
+        self.salt = salt
+        self.accuracy = accuracy # bigger is better
+        self.symbols = tuple(set(string.punctuation) - {'.'})
+
+    def crypt(self, s: str):
+        rsymbols = [random.choice(self.symbols) for _ in range(len(s))]
         
-    def crypt(self, DATA):
-        return ",".join([str(round(ord(i)*self.SALT, self.ACCURACY)) for i in DATA])
+        return "".join(str(round(ord(i) * self.__salt(rsymbols), self.accuracy)) + j for i, j in zip(s, rsymbols))
     
-    def encrypt(self, DATA):
-        return "".join([chr(round(float(i)/self.SALT)) for i in DATA.split(',')])
+    def encrypt(self, s: str):
+        rsymbols = tuple(filter(lambda el: el in self.symbols, s))
+
+        l = "".join(' ' if i in self.symbols else i for i in s).split(' ')
+
+        return "".join(chr(round(float(i) / self.__salt(rsymbols))) 
+                       for i in l if i)
+    
+    def __salt(self, rsymbols):
+        return self.salt + self.__rsymbols_salt(rsymbols)
+        
+    def __rsymbols_salt(self, rsymbols):
+        return sum(map(ord, rsymbols)) / len(rsymbols)
+        
