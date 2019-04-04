2f28ed73180de7b889cb832563e68dbd38a4f0a6
Max Payne <yardalgedal@gmail.com>
2019-04-05 01:27:44 +0300

    Update MMHash.py

--- MMHash.py
+++ MMHash.py
@@ -1,6 +1,6 @@
 from functools import reduce
 
-def MMHash(self, s: str, x: int = 256):
+def MMHash(s: str, x: int = 256):
     L = (x // 8) * 2
     f = lambda ords: str(hex(reduce(lambda x, y: int(x) * int(y),  ords) + int("".join(ords))))
     s = s + str(x)
