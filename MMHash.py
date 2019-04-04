6e08bb7e17919dbc442c5a0077b3f5bf002543b2
Max Payne <yardalgedal@gmail.com>
2019-04-05 01:27:18 +0300

    Update MMHash.py

--- MMHash.py
+++ MMHash.py
@@ -1,21 +1,11 @@
-# Script Name : MMHash.py
-# Author : Yardal Gedal
-# Created : 08 October 2016
-# Last Modified	: 27 August 2017
-# Version : 8.0.0
-
-# Modifications	: 8.0.3
-
-# Description : Hashing input string
-
 from functools import reduce
 
-class MMHash:
-    def __init__(self, S):
-        x = 256
-        L = (x // 8) * 2
-        f = lambda ords: str(hex(reduce(lambda x, y: int(x)*int(y),  ords) + int("".join(ords))))
-        S = S + str(x)
-        while len(S) < L:
-            S = f([str((n + x) * x) + str(ord(i)) for n, i in enumerate(S)])
-        print(S[-L//2:])
+def MMHash(self, s: str, x: int = 256):
+    L = (x // 8) * 2
+    f = lambda ords: str(hex(reduce(lambda x, y: int(x) * int(y),  ords) + int("".join(ords))))
+    s = s + str(x)
+    while len(s) < L:
+        s = f(str((n + x) * x) + str(ord(i)) for n, i in enumerate(s))
+    
+    return s[-L//2:]
+      
